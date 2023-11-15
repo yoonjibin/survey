48fd77d69c97bff72ab1acb3a8f7356116da79ac
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-15 19:01:33 +0900

    add question.graphql

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { QuestionService } from './question.service';
-import { QuestionController } from './question.controller';
 
 @Module({
   providers: [QuestionService],
-  controllers: [QuestionController]
 })
 export class QuestionModule {}
