2473f307bd79e41150ec2cc51731bc874edb84de
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-15 19:27:10 +0900

    add survey.resolver

--- src/survey/survey.module.ts
+++ src/survey/survey.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SurveyService } from './survey.service';
+import { SurveyResolver } from './survey.resolver';
 
 @Module({
-  providers: [SurveyService],
+  providers: [SurveyService, SurveyResolver],
 })
 export class SurveyModule {}
