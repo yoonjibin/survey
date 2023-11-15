464260fa643e7f8d42b65b60025ccaa4c5ed1b2a
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-15 11:43:04 +0900

    add question package

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { SurveyModule } from './survey/survey.module';
+import { QuestionModule } from './question/question.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { SurveyModule } from './survey/survey.module';
       entities: ['dist/**/**.entity{.ts,.js}'],
     }),
     SurveyModule,
+    QuestionModule,
   ],
 })
 export class AppModule {}
