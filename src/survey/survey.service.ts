d66cbe4a3011304f5e0efd3fe3f85e4d643f81ca
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-15 19:59:21 +0900

    add create survey service

--- src/survey/survey.service.ts
+++ src/survey/survey.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SurveyEntity } from 'src/entities/survey.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class SurveyService {}
+export class SurveyService {
+  constructor(
+    @InjectRepository(SurveyEntity)
+    private surveyRepository: Repository<SurveyEntity>,
+  ) {}
+}
