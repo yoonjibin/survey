62155794a9d9e1b86d01248fa0e568f95ffe0d32
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-15 09:17:34 +0900

    add submission entity

---
+++ src/entities/submission.entity.ts
@@ -0,0 +1,11 @@
+import { Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { AnswerEntity } from './answer.entity';
+
+@Entity()
+export class submission {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => AnswerEntity, (AnswerEntity) => AnswerEntity.isCompleted)
+  answer: AnswerEntity;
+}
