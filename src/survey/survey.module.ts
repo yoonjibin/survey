ecc1a04d19c23963a47562bd87d46bb4d2c39c5e
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-15 19:05:14 +0900

    add survey.graphql

--- src/survey/survey.module.ts
+++ src/survey/survey.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { SurveyService } from './survey.service';
-import { SurveyController } from './survey.controller';
 
 @Module({
   providers: [SurveyService],
-  controllers: [SurveyController]
 })
 export class SurveyModule {}
