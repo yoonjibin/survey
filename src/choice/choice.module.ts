b01a78b0c26035a2d676873367c3b2c397c714c3
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-15 19:00:02 +0900

    add choice.graphql

--- src/choice/choice.module.ts
+++ src/choice/choice.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ChoiceService } from './choice.service';
-import { ChoiceController } from './choice.controller';
 
 @Module({
   providers: [ChoiceService],
-  controllers: [ChoiceController],
 })
 export class ChoiceModule {}
