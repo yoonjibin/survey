ab0ac4b6b02606cbd35a2cef4cbbef722bc7b3d6
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-15 11:56:52 +0900

    add proviers controllers in choice module

--- src/choice/choice.module.ts
+++ src/choice/choice.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ChoiceService } from './choice.service';
+import { ChoiceController } from './choice.controller';
 
 @Module({
-  providers: [ChoiceService]
+  providers: [ChoiceService],
+  controllers: [ChoiceController],
 })
 export class ChoiceModule {}
