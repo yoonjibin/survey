261f0f5d5a7c6e030682edcdcfb472790c01e7f5
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-16 00:24:22 +0900

    add getAllSurvey resolver

--- src/survey/survey.resolver.ts
+++ src/survey/survey.resolver.ts
@@ -1,9 +1,14 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { SurveyService } from './survey.service';
 
 @Resolver()
 export class SurveyResolver {
   constructor(private surveyService: SurveyService) {}
+
+  @Query()
+  async getAllSurvey() {
+    return this.surveyService.getAllSurvey();
+  }
 
   @Mutation()
   async createSurvey(@Args('title') title: string) {
