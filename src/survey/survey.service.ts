94f0d0a85545fd711ed0f567320b50e10fdf344f
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-16 08:36:52 +0900

    update updateSurveyServey add notFoundException

--- src/survey/survey.service.ts
+++ src/survey/survey.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SurveyEntity } from 'src/entities/survey.entity';
 import { Repository } from 'typeorm';
@@ -20,6 +20,12 @@ export class SurveyService {
   }
 
   async updateSurvey(id: number, title: string) {
+    if (!this.surveyRepository.exist({ where: { id: id } })) {
+      throw new HttpException(
+        '존재하지 않는 설문지입니다.',
+        HttpStatus.NOT_FOUND,
+      );
+    }
     await this.surveyRepository.update({ id: id }, { title: title });
     return await this.surveyRepository.findOne({ where: { id: id } });
   }
