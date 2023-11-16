0072cce548a54c7f0b483f5d9138d8051d7d08c7
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-16 14:42:03 +0900

    add createChoiceResolver

--- src/choice/choice.resolver.ts
+++ src/choice/choice.resolver.ts
@@ -1,7 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Resolver } from '@nestjs/graphql';
 import { ChoiceService } from './choice.service';
 
 @Resolver()
 export class ChoiceResolver {
   constructor(private choiceService: ChoiceService) {}
+
+  @Mutation('createChoice')
+  async createChoice(
+    @Args('text') text: string,
+    @Args('score') score: number,
+    @Args('questionId', { type: () => Int }) questionId: number,
+  ) {
+    return await this.choiceService.createChoice(text, score, questionId);
+  }
 }
