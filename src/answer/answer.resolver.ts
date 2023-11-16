30912cc76ed4eeed2b043510c5eac7501bba0db7
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-17 08:20:08 +0900

    add createAnswerResolver

--- src/answer/answer.resolver.ts
+++ src/answer/answer.resolver.ts
@@ -1,7 +1,12 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Resolver } from '@nestjs/graphql';
 import { AnswerService } from './answer.service';
 
 @Resolver()
 export class AnswerResolver {
   constructor(private answerService: AnswerService) {}
+
+  @Mutation('createAnswer')
+  async createAnswer(@Args('choiceId', { type: () => Int }) choiceId: number) {
+    return await this.answerService.createAnswer(choiceId);
+  }
 }
