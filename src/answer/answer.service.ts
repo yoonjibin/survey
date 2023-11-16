30be47b1c4c7f1d45f9c58503b83e63c573330e9
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-17 07:57:11 +0900

    add createAnswerService

--- src/answer/answer.service.ts
+++ src/answer/answer.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ChoiceUtil } from 'src/choice/utils/choice.util';
+import { AnswerEntity } from 'src/entities/answer.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AnswerService {}
+export class AnswerService {
+  constructor(
+    private readonly choiceUtil: ChoiceUtil,
+    @InjectRepository(AnswerEntity)
+    private readonly answerRepository: Repository<AnswerEntity>,
+  ) {}
+
+  async createAnswer(choiceId: number) {
+    const choice = await this.choiceUtil.getChoiceById(choiceId);
+    const answer = this.answerRepository.create({ choice: choice });
+
+    return await this.answerRepository.save(answer);
+  }
+}
