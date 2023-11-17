83d960a950516d9ea8e51a68a39eeaaf9b269379
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-17 11:57:40 +0900

    add getAnswerByQuestionIdException

--- src/answer/answer.module.ts
+++ src/answer/answer.module.ts
@@ -5,9 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnswerEntity } from 'src/entities/answer.entity';
 import { ChoiceEntity } from 'src/entities/choice.entity';
 import { ChoiceUtil } from 'src/choice/utils/choice.util';
+import { QuestionEntity } from 'src/entities/question.entity';
+import { QuestionUtil } from 'src/question/utils/question.util';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AnswerEntity, ChoiceEntity])],
-  providers: [AnswerService, AnswerResolver, ChoiceUtil],
+  imports: [
+    TypeOrmModule.forFeature([AnswerEntity, ChoiceEntity, QuestionEntity]),
+  ],
+  providers: [AnswerService, AnswerResolver, ChoiceUtil, QuestionUtil],
 })
 export class AnswerModule {}
--- src/answer/answer.service.ts
+++ src/answer/answer.service.ts
@@ -3,24 +3,35 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { ApolloError } from 'apollo-server-express';
 import { ChoiceUtil } from 'src/choice/utils/choice.util';
 import { AnswerEntity } from 'src/entities/answer.entity';
+import { QuestionUtil } from 'src/question/utils/question.util';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class AnswerService {
   private readonly logger = new Logger(AnswerService.name);
   constructor(
+    private readonly questionUtil: QuestionUtil,
     private readonly choiceUtil: ChoiceUtil,
     @InjectRepository(AnswerEntity)
     private readonly answerRepository: Repository<AnswerEntity>,
   ) {}
 
   async getAllAnswerByQuestionId(questionId: number) {
-    return this.answerRepository
+    await this.questionUtil.checkQuestionExist(questionId);
+    const answer = await this.answerRepository
       .createQueryBuilder('answer')
       .leftJoinAndSelect('answer.choice', 'choice')
       .leftJoinAndSelect('choice.question', 'question')
       .where('question.id = :questionId', { questionId })
       .getOne();
+    if (!answer) {
+      this.logger.error(`Answer with ID ${questionId} not found`);
+      throw new ApolloError('답변이 존재하지 않습니다.', 'NOTFOUND', {
+        customErrorCode: 404,
+        parameter: 'id',
+      });
+    }
+    return answer;
   }
 
   async createAnswer(choiceId: number) {
