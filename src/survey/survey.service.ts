26045afc9de4fdbbb60d3c553c8d88a8197678f2
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-18 16:04:45 +0900

    add updateSurveyCompletedService

--- src/survey/survey.service.ts
+++ src/survey/survey.service.ts
@@ -4,9 +4,11 @@ import { ApolloError } from 'apollo-server-express';
 import { SurveyEntity } from 'src/entities/survey.entity';
 import { Repository } from 'typeorm';
 import { SurveyUtil } from './utils/survey.util';
+import { AnswerEntity } from 'src/entities/answer.entity';
 
 @Injectable()
 export class SurveyService {
+  [x: string]: any;
   constructor(
     private readonly surveyUtil: SurveyUtil,
     @InjectRepository(SurveyEntity)
@@ -31,6 +33,16 @@ export class SurveyService {
     await this.surveyUtil.checkSurveyExist(id);
     await this.surveyRepository.update({ id: id }, { title: title });
     return await this.surveyRepository.findOne({ where: { id: id } });
+  }
+  async updateSurveyCompleted(surveyId: number) {
+    await this.surveyUtil.checkSurveyExist(surveyId);
+    const query = this.createQueryBuilder('question')
+      .leftJoin(AnswerEntity, 'answer', 'answer.questionId = question.id')
+      .where('question.surveyId = :surveyId', { surveyId })
+      .andWhere('answer.id IS NULL');
+    console.log(query);
+    await this.surveyRepository.update({ id: surveyId }, { isCompleted: true });
+    return await this.surveyRepository.findOne({ where: { id: surveyId } });
   }
 
   async deleteSurvey(id: number) {
