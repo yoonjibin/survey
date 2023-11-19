9f19cfe0c9e40bc7d696fd859fb46d6d3fe774df
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-19 15:23:53 +0900

    update checkUnansweredQuestions

--- src/survey/survey.service.ts
+++ src/survey/survey.service.ts
@@ -4,9 +4,11 @@ import { SurveyEntity } from 'src/entities/survey.entity';
 import { Repository } from 'typeorm';
 import { SurveyUtil } from './utils/survey.util';
 import { QuestionEntity } from 'src/entities/question.entity';
+import { ApolloError } from 'apollo-server-express';
 
 @Injectable()
 export class SurveyService {
+  private readonly logger = new Logger(SurveyService.name);
   constructor(
     private readonly surveyUtil: SurveyUtil,
     @InjectRepository(SurveyEntity)
@@ -34,11 +36,8 @@ export class SurveyService {
   }
   async updateSurveyCompleted(surveyId: number) {
     await this.surveyUtil.checkSurveyExist(surveyId);
+    await this.surveyUtil.checkUnansweredQuestions(surveyId);
 
-    const unansweredQuestions = await this.checkUnansweredQuestions(surveyId);
-    if (unansweredQuestions.length > 0) {
-      throw new Error('설문 중 답변하지 않은 질문이 있습니다.');
-    }
     await this.surveyRepository.update({ id: surveyId }, { isCompleted: true });
     return await this.surveyRepository.findOne({ where: { id: surveyId } });
   }
@@ -46,18 +45,5 @@ export class SurveyService {
   async deleteSurvey(id: number) {
     await this.surveyUtil.checkSurveyExist(id);
     await this.surveyRepository.delete({ id: id });
-  }
-
-  async checkUnansweredQuestions(surveyId: number): Promise<QuestionEntity[]> {
-    const unansweredQuestions = await this.surveyRepository
-      .createQueryBuilder('survey')
-      .leftJoinAndSelect('survey.question', 'question')
-      .leftJoin('question.choice', 'choice')
-      .leftJoin('choice.answer', 'answer')
-      .where('survey.id = :surveyId', { surveyId })
-      .andWhere('answer.id IS NULL')
-      .getMany();
-
-    return unansweredQuestions.map((survey) => survey.question).flat();
   }
 }
--- src/survey/utils/survey.util.ts
+++ src/survey/utils/survey.util.ts
@@ -29,4 +29,32 @@ export class SurveyUtil {
     await this.checkSurveyExist(id);
     return this.surveyRepository.findOne({ where: { id: id } });
   }
+
+  async checkUnansweredQuestions(surveyId: number) {
+    const unansweredQuestions = await this.surveyRepository
+      .createQueryBuilder('survey')
+      .leftJoinAndSelect('survey.question', 'question')
+      .leftJoin('question.choice', 'choice')
+      .leftJoin('choice.answer', 'answer')
+      .where('survey.id = :surveyId', { surveyId })
+      .andWhere('answer.id IS NULL')
+      .getMany();
+
+    const unansweredQuestionIdsList = unansweredQuestions
+      .map((survey) => survey.question)
+      .flat();
+    if (unansweredQuestionIdsList.length > 0) {
+      this.logger.error(
+        `No answers found for survey with ID ${unansweredQuestionIdsList[0].id}`,
+      );
+      throw new ApolloError(
+        `${unansweredQuestions[0].id}번 문항에 답변이 존재하지 않습니다.`,
+        'ANSWER_NOT_FOUND',
+        {
+          customErrorCode: 404,
+          parameter: 'id',
+        },
+      );
+    }
+  }
 }
