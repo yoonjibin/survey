dcba11057500d3d67436c7850d1515f535dcb603
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-11-20 13:54:53 +0900

    update question name in survey.graphql

--- src/survey/survey.resolver.ts
+++ src/survey/survey.resolver.ts
@@ -1,6 +1,5 @@
 import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { SurveyService } from './survey.service';
-import { title } from 'process';
 
 @Resolver()
 export class SurveyResolver {
